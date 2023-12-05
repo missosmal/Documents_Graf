723bd024ddae11f5ab43d4a6d45b39d0975caf8d
missosmal <fadesteam8@gmail.com>
2023-12-05 18:55:05 +0500

    Реализованные интерфейсы.Graf

---
+++ Interfaces/IDocument.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documents_Graf.Interfaces
+{
+    public interface IDocument
+    {
+        void Save(bool Update = false);
+
+        List<Documents_Graf.Classes.DocumentContext> AllDocuments();
+        void Delete();
+    }
+}
