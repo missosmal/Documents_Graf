63d0020d04343ad54d4a1f1cbb545d4579a2b472
missosmal <fadesteam8@gmail.com>
2023-12-05 19:43:00 +0500

    Реализованная страница Main.Graf

---
+++ Pages/Main.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Documents_Graf.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Documents_Graf.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Main.xaml
+    /// </summary>
+    public partial class Main : Page
+    {
+        public Main()
+        {
+            InitializeComponent();
+            CreatedUI;
+        }
+        public void Exit(object sender, RoutedEventArgs e)
+        {
+            MainWindow.init.Close();
+        }
+        public void AddDocuments(object sender, RoutedEventArgs e)
+        {
+            MainWindow.init.OpenPages(MainWindow.pages.add);
+        }
+
+        public void CreatedUI()
+        {
+            parent.Children.Clear();
+            foreach(DocumentContext document in MainWindow.init.AllDocuments)
+                parent.Children.Add(new Elements.Item(document));
+        }
+    }
+}
