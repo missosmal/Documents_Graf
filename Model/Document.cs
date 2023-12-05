f6c837a2eaefb9411fafdde912966c3edd08fde9
missosmal <fadesteam8@gmail.com>
2023-12-05 18:51:12 +0500

    Реализованные модели. Graf

---
+++ Model/Document.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Documents_Graf.Model
+{
+    public class Document
+    {
+        public int id {  get; set; }
+        public string src { get; set; }
+        public string name { get; set; }
+        public string user { get; set; }
+        public int id_document { get; set; }
+        public DateTime date { get; set; }
+        public int status { get; set; }
+        public string vector { get; set; }
+    }
+}
